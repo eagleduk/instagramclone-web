851f06ad647f11c4a7fe8cf7aa77a588b525bc7b
SJ.Lee <eagleduk@gmail.com>
2022-06-23 23:31:28 +0900

    LOGIN AND SIGNUP
    Login page

--- src/style.js
+++ src/style.js
@@ -3,7 +3,34 @@ import { reset } from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
     ${reset}
+    * {
+        color: ${(props) => props.theme.color};
+        border-color: ${(props) => props.theme.borderColor};
+        box-sizing: border-box;
+    }
+    body {
+        background-color: ${(props) => props.theme.backgroundColor};
+        font-family: 'Open Sans', sans-serif;
+    }
+    input {
+        all:unset;
+    }
+    a {
+        text-decoration: none;
+    }
 `;
 
-export const lightTheme = {};
-export const darkTheme = {};
+export const lightTheme = {
+  backgroundColor: "#FAFAFA",
+  color: "rgba(0,55,107,1)",
+  borderColor: "rgb(219, 219, 219)",
+  containerColor: "rgb(255, 255, 255)",
+  normalLinkColor: "rgba(0,149,246,1)",
+};
+export const darkTheme = {
+  backgroundColor: "#FAFAFA",
+  color: "rgba(0,55,107,1)",
+  borderColor: "rgb(219, 219, 219)",
+  containerColor: "rgb(255, 255, 255)",
+  normalLinkColor: "rgba(0,149,246,1)",
+};
